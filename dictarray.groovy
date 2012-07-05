b3d5dd4db05e812a867ec1fe17961ab2c56be4f1
Chuck Ross <chivalry@mac.com>
2012-07-04 17:42:43 -0700

    Add comments for all functions in dictarray.groovy.

--- dictarray.groovy
+++ dictarray.groovy
@@ -12,22 +12,31 @@ import groovy.json.JsonSlurper
  ******************************************************************************/
 class Collect {
   /**---------------------------------------------------------------------------
-   * 
+   * Stores the Groovy Map or List that is translated whenever needed.
    */
   def object
+
+  /**---------------------------------------------------------------------------
+   * A builder is used often enough that we simply keep an instance variable.
+   */
   JsonBuilder builder
+
+  /**---------------------------------------------------------------------------
+   * A parser is used often enough that we simply keep an instance variable.
+   */
   JsonSlurper parser
   
+  /**---------------------------------------------------------------------------
+   * All initiators should call this. It should operate as the default
+   * initiator, but I'm unsure how to impliment that in Groovy.
+   */
   def init() {
     builder = new JsonBuilder()
     parser  = new JsonSlurper()
   }
   
   /**---------------------------------------------------------------------------
-   * Empty initiator, this doesn't work without it, don't know enough about
-   * Groovy objects to know why. When I remove it, I get an error when
-   * attempting to create a new Array (subclass of Collect) with a List
-   * parameter.
+   * The initiator for when a dictionary or array is created with no parameters.
    */
   def Collect() {
     this.init()
@@ -146,14 +155,23 @@ class Array extends Collect {
     return this.count() == 0
   }
   
+  /**---------------------------------------------------------------------------
+   * Overloading of the the [] operator for retrieval.
+   */
   Object getAt( Integer index ) {
     return this.value( index )
   }
   
+  /**---------------------------------------------------------------------------
+   * Overloading of the the [] operator for assignment.
+   */
   void putAt( Integer index, Object value ) {
     this.object[ index ] = this.convertValue( value )
   }
   
+  /**---------------------------------------------------------------------------
+   * Overloading of the the == operator for comparison.
+   */
   Boolean equals( Array array ) {
     return this.object == array.object
   }
@@ -167,15 +185,24 @@ class Array extends Collect {
  ******************************************************************************/
 class Dictionary extends Collect {
   
+  /**---------------------------------------------------------------------------
+   * Empty Dictionaries start out as empty Maps.
+   */
   def Dictionary() {
     super.init()
     this.object = [:]
   }
   
+  /**---------------------------------------------------------------------------
+   * Initialize an Dictionary with a the output from another dictionary.
+   */
   def Dictionary( String input ) {
     super( input )
   }
   
+  /**---------------------------------------------------------------------------
+   * Initialize an Dictionary with a Map, converting any values as needed.
+   */
   def Dictionary( Map map ) {
     super.init()
     Map newMap = [:]
@@ -206,14 +233,23 @@ class Dictionary extends Collect {
     return this.count() == 0
   }
   
+  /**---------------------------------------------------------------------------
+   * Overloading of the the [] operator for retrieval.
+   */
   def getAt( String key ) {
     return this.getValueForKey( key )
   }
   
+  /**---------------------------------------------------------------------------
+   * Overloading of the the [] operator for assignment.
+   */
   def setAt( String key, Object value ) {
     this.setValueForKey( key, value )
   }
   
+  /**---------------------------------------------------------------------------
+   * Overloading of the the == operator for comparison.
+   */
   Boolean equals( Dictionary dict ) {
     return this.object == dict.object
   }
@@ -228,6 +264,10 @@ class Dictionary extends Collect {
  ******************************************************************************/
 class FM {
   
+  /**---------------------------------------------------------------------------
+   * If the array parameter is empty, create a new empty array, otherwise use
+   * the string passed
+   */
   static Array createArray( array ) {
     if ( array == '' ) {
       return new Array()
@@ -236,6 +276,10 @@ class FM {
     }
   }
   
+  /**---------------------------------------------------------------------------
+   * If the dict parameter is empty, create a new empty dictionary, otherwise
+   * use the string passed
+   */
   static Dictionary createDict( dict ) {
     if ( dict == '' ) {
       return new Dictionary()
@@ -244,6 +288,10 @@ class FM {
     }
   }
   
+  /**---------------------------------------------------------------------------
+   * If the parameter provided is a list or a map, convert to the JSON
+   * representation.
+   */
   static String convertListOrMap( list_or_map ) {
     if ( ( list_or_map instanceof List )
        | ( list_or_map instanceof Map ) ) {
@@ -252,59 +300,95 @@ class FM {
     return list_or_map
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function arry_Add.
+   */
   static String arry_Add( array, value ) {
     Array newArray = this.createArray( array )
     newArray.add( value )
     return newArray.output
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function arry_Count.
+   */
   static Integer arry_Count( array ) {
     Array newArray = this.createArray( array )
     return newArray.count()
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function arry_Head.
+   */
   static String arry_Head( array ) {
     Array newArray = this.createArray( array )
     return newArray.head()
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function arry_IsEmpty.
+   */
   static Boolean arry_IsEmpty( array ) {
     Array newArray = this.createArray( array )
     return newArray.isEmpty()
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function arry_Tail.
+   */
   static String arry_Tail( array ) {
     Array newArray = this.createArray( array )
     return newArray.tail().output
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function arry_Value.
+   */
   static String arry_Value( array, index ) {
     Array newArray = this.createArray( array )
     def value = newArray.value( index.toInteger() )
     return this.convertListOrMap( value )
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function dict_Count.
+   */
   static Integer dict_Count( dict ) {
     Dictionary newDict = this.createDict( dict )
     return newDict.count()
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function
+   * dict_GetValueForKey.
+   */
   static String dict_GetValueForKey( dict, key ) {
     Dictionary newDict = this.createDict( dict )
     def value = newDict.getValueForKey( key )
     return this.convertListOrMap( value )
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function dict_IsEmpty.
+   */
   static Boolean dict_IsEmpty( dict ) {
     Dictionary newDict = this.createDict( dict )
     return newDict.isEmpty()
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function
+   * dict_RemoveKey.
+   */
   static String dict_RemoveKey( dict, key ) {
     Dictionary newDict = this.createDict( dict )
     return newDict.remove( key ).output
   }
   
+  /**---------------------------------------------------------------------------
+   * An exact analog to the anticipated FileMaker custom function
+   * dict_SetValueForKey.
+   */
   static String dict_SetValueForKey( dict, key, value ) {
     Dictionary newDict = this.createDict( dict )
     newDict.setValueForKey( key, value )
